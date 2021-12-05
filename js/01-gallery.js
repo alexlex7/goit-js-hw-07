3a0ef30771b899d40b714f46f9864ae287861ffc
alexlex7 <alekslosev7@gmail.com>
2021-12-05 15:08:10 +0200

    added lazy loading

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const items = Array(50)
+  .fill("")
+  .map((item, index) => {
+    return {
+      preview: `https://picsum.photos/seed/picsum${index}/510/340?random=${index}`,
+      original: `https://picsum.photos/seed/picsum${index}/1280/854?random=${index}`,
+      description: "Hokkaido Flower",
+    };
+  });
+
+const newGalleryItems = galleryItems.concat(items);
+console.log("~ newGalleryItems", newGalleryItems);
+
 const galleryEl = document.querySelector(".gallery");
 const galleryMarkup = createGalleryMarkup(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 function createGalleryMarkup(galleryItems) {
-  const markup = galleryItems
+  const markup = newGalleryItems
     .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
   <a class="gallery__link"  href="${original}">
     <img
+      loading="lazy"
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
